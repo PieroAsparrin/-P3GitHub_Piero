8a6bfa22ae89a4053573890898268381fe22ee6f
Piero Gianfranco Asparrin Vásquez <pieroasparrinvasquez@gmail.com>
2024-03-08 11:38:19 +0100

    Aplicacion v1.1
    Añadi un bucle para que el arrayList pueda guardar mas de un nombre, implemente un menu sencillo que permita salir del programa o volver a la parte en la que se solicita un nombre.

--- Aplicacion.java
+++ Aplicacion.java
@@ -3,16 +3,29 @@ import java.util.Scanner;
 
 public class Aplicacion {
     static Scanner entrada = new Scanner(System.in);
-    static ArrayList <String> nombres = new ArrayList<>();
-    public static void main(String[] args) {
-    
-        System.out.println("Este programa guarda nombres:");
+    static ArrayList<String> nombres = new ArrayList<>();
 
-        System.out.println("Escribe tu nombre:");
-        String nombre = entrada.nextLine();
-        nombres.add(nombre);
-        System.out.println("El nombre " + nombre + " se ha guardado correctamente.");
+    public static void main(String[] args) {
+        boolean salirBucle = false;
 
         
+            System.out.println("Este programa guarda nombres:");
+
+        do {
+            System.out.println("Escribe un nombre:");
+            String nombre;
+            nombre = entrada.nextLine();
+            nombres.add(nombre);
+            System.out.println("El nombre " + nombre + " se ha guardado correctamente.");
+
+            System.out.println("¿Desea guardar otro nombre?\n1.Si\n2.No");
+            int eleccion = entrada.nextInt();
+            if (eleccion == 1) {
+                entrada.nextLine();
+                System.out.println("");
+            }else if (eleccion == 2) {
+                salirBucle = true;
+            }
+        } while (!salirBucle);
     }
 }
